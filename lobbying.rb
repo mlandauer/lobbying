a2f064693408e53209d3de3155c2bb79fcc836fd
Matthew Landauer <matthew@openaustralia.org>
2009-08-23 17:07:40 +1000

    Don't need to put quotes around csv values

--- lobbying.rb
+++ lobbying.rb
@@ -52,7 +52,7 @@ end
 # For each lobbyist get their clients and output the result
 # On the individual lobbyist page the whole layout is done with tables. Not pretty at all. Who wrote this?
 File.open("lobbying.csv", "w") do |f|
-  f.puts '"Business Entity Name", "Trading Name", ABN, Clients'
+  f.puts 'Business Entity Name,Trading Name,ABN,Clients'
   lobbyists.each do |l|
     page = get_page("#{base_url}?event=viewProfile&profileID=#{l[:profile_id]}")
     clients = []
@@ -60,8 +60,7 @@ File.open("lobbying.csv", "w") do |f|
       clients << tr.search("td")[1].inner_text.strip
     end
 
-    clients_with_quotes = clients.map{|c| "\"#{c}\""}
-    f.puts "\"#{l[:entity]}\", \"#{l[:trading_name]}\", #{l[:abn]}, #{clients_with_quotes.join(', ')}"
+    f.puts "#{l[:entity]},#{l[:trading_name]},#{l[:abn]},#{clients.join(', ')}"
     f.flush
   end
 end
