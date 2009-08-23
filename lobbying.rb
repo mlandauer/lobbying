a602005763c15baa2e7f413de92ff4d6307af526
Matthew Landauer <matthew@openaustralia.org>
2009-08-23 16:50:18 +1000

    Writes to file. Added retries on http errors.

--- lobbying.rb
+++ lobbying.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #
 # Downloads data from the Australian Federal Lobbyists Register and output
-# as a "comma separated values" (CSV) to load into a spreadsheet
+# as a "comma separated values" (CSV) file to load into a spreadsheet
 # Copyright (C) 2009 OpenAustralia Foundation
 #
 # This program is free software; you can redistribute it and/or
@@ -15,14 +15,31 @@
 # GNU General Public License for more details.
 #
 # Author: Matthew Landauer
+#
+# This code brought to you by the following conversation at 10:25am, Sunday morning, 23 August 2009:
+# Kat Szuminska: "How about we submit a Perl app to the taskforce as an essay in transparency?"
+# Matthew Landauer: "Okay... but I'll write it in Ruby"
 
 require 'rubygems'
 require 'hpricot'
 require 'open-uri'
 
-url = "http://lobbyists.pmc.gov.au/lobbyistsregister/index.cfm"
+base_url = "http://lobbyists.pmc.gov.au/lobbyistsregister/index.cfm"
 
-page = Hpricot(open("#{url}?event=whoIsOnRegister"))
+def get_page(url)
+  sleep_time = 1
+  begin
+    Hpricot(open(url))
+  rescue OpenURI::HTTPError, Timeout::Error
+    puts "Had trouble downloading the page #{url} with the error #{$!}. retrying in #{sleep_time}s..."
+    sleep(sleep_time)
+    puts "Retrying now"
+    sleep_time *= 2
+    retry
+  end
+end
+
+page = get_page("#{base_url}?event=whoIsOnRegister")
 
 lobbyists = []
 # Skip first row of table because it contains the headings
@@ -33,14 +50,18 @@ page.search("table > tr")[1..-1].each do |row|
 end
 
 # For each lobbyist get their clients and output the result
-puts '"Business Entity Name", "Trading Name", ABN, Clients'
-lobbyists.each do |l|
-  page = Hpricot(open("#{url}?event=viewProfile&profileID=#{l[:profile_id]}"))
-  clients = []
-  page.search("table")[5].search("tr")[1..-2].each do |tr|
-    clients << tr.search("td")[1].inner_text.strip
-  end
+# On the individual lobbyist page the whole layout is done with tables. Not pretty at all. Who wrote this?
+File.open("lobbying.csv", "w") do |f|
+  f.puts '"Business Entity Name", "Trading Name", ABN, Clients'
+  lobbyists.each do |l|
+    page = get_page("#{base_url}?event=viewProfile&profileID=#{l[:profile_id]}")
+    clients = []
+    page.search("table")[5].search("tr")[1..-2].each do |tr|
+      clients << tr.search("td")[1].inner_text.strip
+    end
 
-  clients_with_quotes = clients.map{|c| "\"#{c}\""}
-  puts "\"#{l[:entity]}\", \"#{l[:trading_name]}\", #{l[:abn]}, #{clients_with_quotes.join(', ')}"
+    clients_with_quotes = clients.map{|c| "\"#{c}\""}
+    f.puts "\"#{l[:entity]}\", \"#{l[:trading_name]}\", #{l[:abn]}, #{clients_with_quotes.join(', ')}"
+    f.flush
+  end
 end
