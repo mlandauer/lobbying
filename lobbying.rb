bd25220519d43225103e0f02c8dee2ed702bb376
Matthew Landauer <matthew@openaustralia.org>
2009-08-23 11:22:51 +1000

    Separate collection and output

--- lobbying.rb
+++ lobbying.rb
@@ -10,11 +10,14 @@ require 'open-uri'
 
 page = Hpricot(open("http://lobbyists.pmc.gov.au/lobbyistsregister/index.cfm?event=whoIsOnRegister"))
 
-puts '"Business Entity Name", "Trading Name", ABN'
+data = []
 # Skip first row of table because it contains the headings
 page.search("table > tr")[1..-1].each do |row|
-  business_entity_name = (row.search("td")[1]/"a").inner_text
-  trading_name = (row.search("td")[2]/"a").inner_text
-  abn = row.search("td")[3].inner_text.strip
-  puts "\"#{business_entity_name}\", \"#{trading_name}\", #{abn}"
+  td = row.search("td")
+  data << {:entity => (td[1]/"a").inner_text, :trading_name => (td[2]/"a").inner_text, :abn => td[3].inner_text.strip}
+end
+
+puts '"Business Entity Name", "Trading Name", ABN'
+data.each do |a|
+  puts "\"#{a[:entity]}\", \"#{a[:trading_name]}\", #{a[:abn]}"
 end
