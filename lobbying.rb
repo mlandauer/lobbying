159c210c231651524dbef3979629a9f357e11f73
Matthew Landauer <matthew@openaustralia.org>
2009-08-23 11:53:18 +1000

    Now gets the client too!

--- lobbying.rb
+++ lobbying.rb
@@ -8,16 +8,27 @@ require 'rubygems'
 require 'hpricot'
 require 'open-uri'
 
-page = Hpricot(open("http://lobbyists.pmc.gov.au/lobbyistsregister/index.cfm?event=whoIsOnRegister"))
+url = "http://lobbyists.pmc.gov.au/lobbyistsregister/index.cfm"
 
-data = []
+page = Hpricot(open("#{url}?event=whoIsOnRegister"))
+
+lobbyists = []
 # Skip first row of table because it contains the headings
 page.search("table > tr")[1..-1].each do |row|
   td = row.search("td")
-  data << {:entity => (td[1]/"a").inner_text, :trading_name => (td[2]/"a").inner_text, :abn => td[3].inner_text.strip}
+  lobbyists << {:profile_id => td[0].at("input")["value"], :entity => (td[1]/"a").inner_text,
+    :trading_name => (td[2]/"a").inner_text, :abn => td[3].inner_text.strip}
 end
 
-puts '"Business Entity Name", "Trading Name", ABN'
-data.each do |a|
-  puts "\"#{a[:entity]}\", \"#{a[:trading_name]}\", #{a[:abn]}"
+# For each lobbyist get their clients and output the result
+puts '"Business Entity Name", "Trading Name", ABN, Clients'
+lobbyists.each do |l|
+  page = Hpricot(open("#{url}?event=viewProfile&profileID=#{l[:profile_id]}"))
+  clients = []
+  page.search("table")[5].search("tr")[1..-2].each do |tr|
+    clients << tr.search("td")[1].inner_text.strip
+  end
+
+  clients_with_quotes = clients.map{|c| "\"#{c}\""}
+  puts "\"#{l[:entity]}\", \"#{l[:trading_name]}\", #{l[:abn]}, #{clients_with_quotes.join(', ')}"
 end
